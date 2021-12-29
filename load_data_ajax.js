f6173689c3876ab40f574864084267a9e60b6221
Rojson <michal.roj99@gmail.com>
2021-12-29 10:01:32 +0100

    query fix

--- load_data_ajax.js
+++ load_data_ajax.js
@@ -1,5 +1,5 @@
 var ikona = "";
-var query = "SELECT * FROM obrazki join uzytkownicy using(id_uzytkownika) join obrazek_kategorie USING(id_obrazka) join kategorie USING(id_kategorii) order by data_wygasniecia";
+var query = "SELECT *, TIMESTAMPDIFF(minute, NOW(),data_wygasniecia)as'zostalo' FROM obrazki join uzytkownicy using(id_uzytkownika) join obrazek_kategorie USING(id_obrazka) join kategorie USING(id_kategorii) order by zostalo";
 //console.log(query);
 document.onload = new function () {
 	function show(query)
@@ -25,7 +25,7 @@ document.onload = new function () {
 				}else
 				{
 					ik.push(tab[i].nazwa_kategorii);
-					ik.push(tab[i].id_obrazka);
+					//ik.push(tab[i].id_obrazka);
 					console.log(ik);
 					
 					ikona = ikona + '<div class="szot"><div class="szot__nick szot--text">#' + tab[i].login +'</div><div class="szot__tags horizontal-container">';
@@ -34,7 +34,7 @@ document.onload = new function () {
 						ikona = ikona + '<div class="szot__tag szot--text">#'+ik[x]+'</div>';
 					}
 
-					ikona = ikona + '</div><img src="'+tab[i].obrazek+'"><div class="szot__title">'+tab[i].tytul+'</div><div class="szot__desc">'+tab[i].opis+'</div><div class="szot__like_plus horizontal-container"><form><input type="submit" value="" class="plus_btn"></form>22</div><div class="szot__time">Ważny do: 21'+ tab[i].data_wygasniecia+'</div></div>';
+					ikona = ikona + '</div><img src="'+tab[i].obrazek+'"><div class="szot__title">'+tab[i].tytul+'</div><div class="szot__desc">'+tab[i].opis+'</div><div class="szot__like_plus horizontal-container"><form><input type="submit" value="" class="plus_btn"></form>22</div><div class="szot__time">Minuty do wygaśnięcia: '+ tab[i].zostalo+'</div></div>';
 					
 					ik=[];
 				}
@@ -59,7 +59,7 @@ document.onload = new function () {
 	$(document).ready(function() {
 		$("#author_search").click(function() {
 			var name = document.getElementById("author_name").value;
-			query = "SELECT * FROM obrazki join uzytkownicy using(id_uzytkownika) join obrazek_kategorie USING(id_obrazka) join kategorie USING(id_kategorii) where login = '"+name+"' order by data_wygasniecia";
+			query = "SELECT *,TIMESTAMPDIFF(minute, NOW(),data_wygasniecia)as'zostalo' FROM obrazki join uzytkownicy using(id_uzytkownika) join obrazek_kategorie USING(id_obrazka) join kategorie USING(id_kategorii) where login = '"+name+"' order by zostalo";
 			console.log(query);
 			show(query);
 		});
@@ -69,7 +69,7 @@ document.onload = new function () {
 			var chceck_krajobraz = document.getElementById("chceck-krajobraz");
 			var chceck_sztuka = document.getElementById("chceck-sztuka");
 	
-			query = "SELECT * FROM obrazki join uzytkownicy using(id_uzytkownika) join obrazek_kategorie USING(id_obrazka) join kategorie USING(id_kategorii)";
+			query = "SELECT *,TIMESTAMPDIFF(minute, NOW(),data_wygasniecia)as'zostalo' FROM obrazki join uzytkownicy using(id_uzytkownika) join obrazek_kategorie USING(id_obrazka) join kategorie USING(id_kategorii)";
 			var ifchceck=0;
 			if (chceck_smieszne.checked == true)
 			{
@@ -102,7 +102,7 @@ document.onload = new function () {
 			query+=")";
 	
 	
-			endofquery = "order by data_wygasniecia;";
+			endofquery = "order by zostalo;";
 			fullquery = query + endofquery;
 			console.log(fullquery);
 			show(fullquery);		
