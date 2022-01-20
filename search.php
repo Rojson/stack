a367ad70ae07b3e5c254cb3fdac7f6a42fabeda3
Rojson <michal.roj99@gmail.com>
2022-01-20 20:59:01 +0100

    mobile nav

--- search.php
+++ search.php
@@ -8,9 +8,11 @@
     <link rel='stylesheet' href="css/search.css">
     <link rel='stylesheet' href="css/log.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/log.js"></script>
     <script src="load_data_ajax.js"></script>
 </head>
@@ -40,37 +42,43 @@
 
         require("like.php");
     ?>
-    <nav class="nav fixed-top container-fluid">
-        <div class="horizontal-container width">
+    <nav class="navbar nav navbar-expand-md fixed-top container-fluid navbar-dark">
             <img src="css/logo.png">
-            <div class="horizontal-container nav-container__right-side">
-                <a href="index.php" class="pointer">Najnowsze</a>
-                <a href="search.php" class="pointer nav-active">Szukaj</a>
-                <a href="add.php" class="pointer">Wstaw</a>
-                <?php
-                    if(isset($_SESSION['username']))
-                    { 
-                        echo '<span class="logged_user">#'.$_SESSION['username'].'</span>';
-                    }else
-                    {
-                        echo '<span id="toggle_log" class="nav-button nav-button__first-button pointer">Zaloguj się</span>';
-                    }
-                 ?>
-                
-                <span class="nav-sep"></span>
-                <?php
-                    if(isset($_SESSION['username']))
-                    { 
-                        echo '<form action="" method="POST">';
-                        echo '<input type="submit" name="logout" class="nav-button pointer" value="Wyloguj się">';
-                        echo '</form>';
-                    }else
-                    {
-                        echo '<span id="toggle_sign" class="nav-button pointer">Zarejestruj się</span>';
-                    }
-                 ?>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div id="collapsibleNavbar" class="justify-content-end collapse navbar-collapse">
+                <div class="navbar-nav horizontal-container nav-container__right-side">
+                    <a href="index.php" class="pointer nav-active">Najnowsze</a>
+                    <a href="search.php" class="pointer">Szukaj</a>
+                    <a href="add.php" class="pointer">Wstaw </a>
+                    <?php
+                        if(isset($_SESSION['username']))
+                        { 
+                            echo '<span class="logged_user">#'.$_SESSION['username'].'</span>';
+                        }else
+                        {
+                            echo '<span id="toggle_log" class="nav-button nav-button__first-button pointer">Zaloguj się</span>';
+                        }
+                    ?>
+                    <span class="nav-sep"></span>
+                    <?php
+                        if(isset($_SESSION['username']))
+                        { 
+                            echo '<form action="" method="POST">';
+                            echo '<input type="submit" name="logout" class="nav-button pointer" value="Wyloguj się">';
+                            echo '</form>';
+                        }else
+                        {
+                            echo '<span id="toggle_sign" class="nav-button pointer">Zarejestruj się</span>';
+                        }
+                    ?>
+                    
+                </div>
             </div>
-        </div>
+
     </nav>
     <div class="szot_container width horizontal-container margin">
         <div class="search_container">
